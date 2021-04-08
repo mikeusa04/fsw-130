b66910d48bf6809165587117dc8e3d7cab292a2f
mikeusa04 <71799572+mikeusa04@users.noreply.github.com>
2021-04-08 07:28:33 -0700

    week3 assignment

--- week2/assignment1/my-app/src/App.js
+++ week2/assignment1/my-app/src/App.js
@@ -1,4 +1,18 @@
-/*import React,{useState} from "react"
+/*You are going to build a React app that manages an array of objects (Ex: an array of movies, TV shows, animals, etc.) in context state.
+Provide that state to your application and map out the array to the DOM.
+Ex: If you do movies, the array of movies should appear on the webpage when the page loads
+Create a method in context that allows you to add a new item to the array, then build the appropriate components that will use that method, (you will need to build a form component that consumes your context for this method)
+When you add a new item through the form, it should be added to the array in context state and appear on the webpage with the rest of the initial data.
+Fully style this webpage and make it responsive.
+Requirements
+Website displays an array of data that is managed using context.
+Fully styled in CSS and responsive
+You can add a new item to the array using a form
+
+
+
+
+import React,{useState} from "react"
 import './App.css';
 
 function App() {
---
+++ week3/assignment1/my-app/src/App.js
@@ -0,0 +1,44 @@
+/* 
+use redux to:
+Display an initial count.
+Display a button whose onclick action will be to increment or decrement the counter using Redux.
+Each button click should update the initial count.
+
+to install redux:
+npm install redux react-redux --save then hit enter*/
+
+
+import Counter from "./Counter"
+import './App.css';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const initialState = {
+  count: 0
+};
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;    
+  }
+}
+const store = createStore(reducer);
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Counter />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
